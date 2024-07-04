9d5afaf958b57a7ce77998d6612123a3c220e7d9
Tap1ta <wolfteamsex1@gmail.com>
2024-07-04 00:31:34 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,22 +5,25 @@ dotenv.config();
 const app = express();
 
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-
-io.on('connection', client => {
-    console.log(`connection recieved`);
-    client.on('new_message', (chat) => {
-        console.log(`new message recieved: ${chat}`)
-        io.emit('broadcast', chat)
-    })
-})
-
-app.get('/', (req, res) => {
-    res.send('Server is running')
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+    }
 });
 
+io.on('connection', client => {
+    console.log(`connection received`);
+    client.on('new_message', (chat) => {
+        console.log(`new message received: ${chat}`)
+        io.emit('broadcast', chat)
+    })
+});
 
-const port = process.env.PORT;
-server.listen(port, () => {
-    console.log(`server running at ${port}...`)
-})
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, '0.0.0.0', () => {
+    console.log(`server running at ${port}...`);
+});
