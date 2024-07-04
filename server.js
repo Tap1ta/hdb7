307fe4f9794aba0ca8a251e1da8391bcb71ad77c
Tap1ta <wolfteamsex1@gmail.com>
2024-07-04 01:43:59 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,9 @@
-const fs = require('fs');
-const https = require('https');
 const express = require('express');
 const app = express();
 
-// Certificados SSL
-const privateKey = fs.readFileSync('path/to/your/private-key.pem', 'utf8');
-const certificate = fs.readFileSync('path/to/your/certificate.pem', 'utf8');
-const ca = fs.readFileSync('path/to/your/ca.pem', 'utf8');
+const port = 3000; // Configuración directa del puerto 3000
 
-const credentials = {
-    key: privateKey,
-    cert: certificate,
-    ca: ca
-};
-
-const port = 3000;
-
-const server = https.createServer(credentials, app);
+const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 io.on('connection', client => {
