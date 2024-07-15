62055f12d0b72bc8eee99ccb364d3a80a516b853
Tap1ta <wolfteamsex1@gmail.com>
2024-07-15 02:08:58 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'https://culturar-git-main-esquema-rs-projects.vercel.app/',
+    origin: 'https://culturar.vercel.app/',
     methods: ['GET', 'POST']
   }
 });
