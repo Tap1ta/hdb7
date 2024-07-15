e47c2a5018a5752fccab670a37bf61fdc7abbec9
tapita <wolfteamsex1@gmail.com>
2024-07-15 01:07:00 -0500

    f

--- server/index.js
+++ server/index.js
@@ -7,6 +7,12 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, push } from 'firebase/database';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+console.log(__dirname); // Now you can use __dirname in your ES module
 
 dotenv.config();
 
